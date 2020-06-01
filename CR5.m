9a894c064fe40e0e1f20d3bc4f1888b8796f1c42
giath <giath@Tim-15R3.modem>
2020-06-01 12:43:17 +1000

    Update teach function

--- CR5.m
+++ CR5.m
@@ -13,15 +13,14 @@ classdef  CR5<handle
         
         function GetCR5(self)
             
-            L(1) = Link('d',0.147,'a',0,'alpha',pi/2,'offset',0,'qlim',[-deg2rad(360) deg2rad(360)]);
-            L(2) = Link('d',0.025,'a',0.427,'alpha',0,'offset',0,'qlim',[-deg2rad(360) deg2rad(360)]);
+            L(1) = Link('d',0.147,'a',0,'alpha',pi/2,'offset',0,'qlim',[-deg2rad(180) deg2rad(180)]);
+            L(2) = Link('d',0.025,'a',0.427,'alpha',0,'offset',0,'qlim',[-deg2rad(180) deg2rad(180)]);
             L(3) = Link('d',0,'a',0.357,'alpha',0,'offset',0,'qlim',[-deg2rad(160) deg2rad(160)]);
-            L(4) = Link('d',0.116,'a',0,'alpha',pi/2,'offset',pi/2,'qlim',[-deg2rad(360) deg2rad(360)]);
-            L(5) = Link('d',0.116,'a',0,'alpha',-pi/2,'offset',0,'qlim',[-deg2rad(360) deg2rad(360)]);
+            L(4) = Link('d',0.116,'a',0,'alpha',pi/2,'offset',pi/2,'qlim',[-deg2rad(180) deg2rad(180)]);
+            L(5) = Link('d',0.116,'a',0,'alpha',-pi/2,'offset',0,'qlim',[-deg2rad(180) deg2rad(180)]);
             L(6) = Link('d',0.114,'a',0,'alpha',0,'offset',0,'qlim',[-deg2rad(360) deg2rad(360)]);
             
-            self.model = SerialLink(L,'name',['CR5_',datestr(now,'yyyymmddTHHMMSSFFF')]);
-
+            self.model = SerialLink(L,'name',['CR5']);
             
         end
         %     function PlotAndColour()
@@ -33,7 +32,7 @@ classdef  CR5<handle
         % Given a robot index, add the glyphs (vertices and faces) and
         % colour them in if data is available
         function PlotAndColourRobot(self)
-            q= [0 0 0 0 0 0] 
+            q= [0 0 0 0 0 0] ;
             self.model.plot(q);
         end
 %         function PlotAndColourRobot(self)%robot,workspace)
